ddfe5a23bce6553e09fd6156e171f433bafaa59f
Fasz0711 <fabrizio0711@hotmail.com>
2025-02-18 20:04:38 -0500

    refactor: change api endpoint to https

--- src/components/useFetchWeather.js
+++ src/components/useFetchWeather.js
@@ -4,7 +4,7 @@ export const useFetchWeather = (city) => {
   const [data, setData] = useState([]);
   const [weatherImage, setWeatherImage] = useState("");
   const apiKey = "cec0491d4ad353c057fe29950098e02b";
-  const endpoint1 = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}&units=metric`;
+  const endpoint1 = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}&units=metric`;
 
   const fetchWeather = async () => {
     //GET LAT AND LON
